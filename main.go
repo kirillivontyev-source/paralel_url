c76b03dc97c2d1c76c751b372e07b67511a3310d
kirillivontev <kirillivontyev@gmail.com>
2026-04-20 22:28:34 +0300

    final

--- main.go
+++ main.go
@@ -1,5 +1,71 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"sync"
+)
+
+func worker(wg *sync.WaitGroup, ch <-chan string) {
+
+	defer wg.Done()
+
+	for url := range ch {
+
+		v, err := http.Get(url)
+		if err != nil {
+			fmt.Println(err.Error())
+			continue
+		}
+
+		defer v.Body.Close()
+
+		b, err := io.ReadAll(v.Body)
+		if err != nil {
+			fmt.Println(err.Error())
+			continue
+		}
+
+		fmt.Printf("url: %+v | byte: %+v \n", url, len(b))
+
+	}
+
+}
+
 func main() {
+
+	sUrl := []string{
+		"https://google.com",
+		"https://youtube.com",
+		"https://github.com",
+		"https://Facebook.com",
+		"https://Instagram.com",
+		"https://chatgpt.com",
+	}
+
+	wg := sync.WaitGroup{}
+
+	chUrl := make(chan string)
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for _, v := range sUrl {
+			chUrl <- v
+		}
+
+		close(chUrl)
+
+	}()
+
+	wg.Add(1)
+	go worker(&wg, chUrl)
+	wg.Add(1)
+	go worker(&wg, chUrl)
+	wg.Add(1)
+	go worker(&wg, chUrl)
+
+	wg.Wait()
 
 }
